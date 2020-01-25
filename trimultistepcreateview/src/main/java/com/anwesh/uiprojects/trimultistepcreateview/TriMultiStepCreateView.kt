bf99b33accdfc2e635fcf50e156416df5618654e
Anwesh43 <anweshthecool0@gmail.com>
2020-01-25 12:34:21 +0530

    adding TriMutliStepView class

--- trimultistepcreateview/src/main/java/com/anwesh/uiprojects/trimultistepcreateview/TriMultiStepCreateView.kt
+++ trimultistepcreateview/src/main/java/com/anwesh/uiprojects/trimultistepcreateview/TriMultiStepCreateView.kt
@@ -60,3 +60,20 @@ fun Canvas.drawTSLNode(i : Int, scale : Float, paint : Paint) {
     restore()
 }
 
+class TriMultiStepCreateView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
