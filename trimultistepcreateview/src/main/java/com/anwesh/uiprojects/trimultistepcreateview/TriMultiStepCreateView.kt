02fda1e7e867a4da75fa19677c024299c73ca6bf
Anwesh43 <anweshthecool0@gmail.com>
2020-01-25 12:46:30 +0530

    adding TriMultiStep class

--- trimultistepcreateview/src/main/java/com/anwesh/uiprojects/trimultistepcreateview/TriMultiStepCreateView.kt
+++ trimultistepcreateview/src/main/java/com/anwesh/uiprojects/trimultistepcreateview/TriMultiStepCreateView.kt
@@ -165,4 +165,26 @@ class TriMultiStepCreateView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class TriMultiStep(var i : Int) {
+
+        private var curr : TSLNode = TSLNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
