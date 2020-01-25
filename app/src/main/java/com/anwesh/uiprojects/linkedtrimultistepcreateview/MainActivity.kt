dc0e08bd7f4514502e8b29642c66586b3b1d2594
Anwesh43 <anweshthecool0@gmail.com>
2020-01-25 12:56:55 +0530

    adding view and fullscreen method

--- app/src/main/java/com/anwesh/uiprojects/linkedtrimultistepcreateview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedtrimultistepcreateview/MainActivity.kt
@@ -2,11 +2,20 @@ package com.anwesh.uiprojects.linkedtrimultistepcreateview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.anwesh.uiprojects.trimultistepcreateview.TriMultiStepCreateView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        TriMultiStepCreateView.create(this)
+        fullScreen()
     }
+}
+
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
