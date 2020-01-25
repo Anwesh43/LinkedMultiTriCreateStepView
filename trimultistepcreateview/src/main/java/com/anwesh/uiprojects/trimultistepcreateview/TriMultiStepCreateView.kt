7811999bcca1eb6fd2d0f037023a5df538539a8a
Anwesh43 <anweshthecool0@gmail.com>
2020-01-25 12:04:35 +0530

    adding constants and libraries

---
+++ trimultistepcreateview/src/main/java/com/anwesh/uiprojects/trimultistepcreateview/TriMultiStepCreateView.kt
@@ -0,0 +1,22 @@
+package com.anwesh.uiprojects.trimultistepcreateview
+
+/**
+ * Created by anweshmishra on 25/01/20.
+ */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Color
+import android.content.Context
+import android.app.Activity
+
+val nodes : Int = 5
+val lines : Int = 5
+val scGap : Float = 0.02f
+val strokeFactor : Float = 90f
+val delay : Long = 20
+val foreColor : Int = Color.parseColor("#3F51B5")
+val backColor : Int = Color.parseColor("#BDBDBD")
+
